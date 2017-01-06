461cd0d3b4a048cb69aea3c74701544a25c7b7ab
DESKTOP-1VJF0IT\Maicius <maicius@outlook.com>
2017-01-07 02:44:18 +0800

    栈帧的画法，关于保存的ebp的值还是不懂

---
+++ Week_StackFrame/pofile.cpp
@@ -0,0 +1,15 @@
+/***
+#include <stdio.h>
+void main() {
+int i,j;
+float a[100];
+for (i = 0; i <100; i++)
+a[i] =1.0;
+for(i=0;j<100;j++){
+for (j=1;j<=100;i++){
+a[i] = a[j];
+
+      	}
+   	}
+}
+*/
