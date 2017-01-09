992979b924997aa8b91c4b168c3087422558441f
DESKTOP-1VJF0IT\Maicius <maicius@outlook.com>
2017-01-09 23:28:21 +0800

    指针练习

--- week02/SystemProgrammingWeek02Test/PointerTest2/main.cpp
+++ week02/SystemProgrammingWeek02Test/PointerTest2/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string>
-int counter=0;
+//int counter=0;
 //int* ptr_to_zero(){
 //	int i=0;
 //	return &i;
@@ -25,20 +25,63 @@ int counter=0;
 //	strcpy(str,"hello");
 //	getchar();
 //}
-int f(int);
-int func1(int x){
-	return f(x)+f(x)+f(x)+f(x);
-}
-int func2(int x){
-	return 4*f(x);
-}
+//int f(int);
+//int func1(int x){
+//	return f(x)+f(x)+f(x)+f(x);
+//}
+//int func2(int x){
+//	return 4*f(x);
+//}
+//int main(){
+//	//int a=func1(1);
+//	int b=func2(1);
+//	//printf("func1:%d\n", a);
+//	printf("fcun2:%d\n", b);
+//	getchar();
+//}
+//int f(int x){
+//	return counter++;
+//}
+//int main(){
+//	char a[100];
+//	a[99]=*((char *)(((int)&a[0])+4));
+//	return 0;
+//}
+//int call(int *count){
+//	*count = *count+1;
+//	return *count;
+//}
+//int main()
+//{
+//	int count =4;
+//	int re;
+//	re=call(&count);
+//	printf("%d",re);
+//	return 0;
+////}
+//int i;
+//int *jp=&i;
+//void main()
+//{
+//	printf("%d,%d\n",(int)&i,(int)jp);
+//	getchar();
+//}
+//static int init=0;
+//static int uinit;
+//int square(int *arg)
+//{
+//	int n =*arg;
+//	return n*n;
+//}
+//int main(int argc, char *argv[]){
+//    int init_test;
+//	//uinit=1;
+//	int arg=strtol(argv[1],NULL,0);
+//	return square(&arg);
+//}
 int main(){
-	//int a=func1(1);
-	int b=func2(1);
-	//printf("func1:%d\n", a);
-	printf("fcun2:%d\n", b);
-	getchar();
-}
-int f(int x){
-	return counter++;
+	int n=3;
+	char str[10]="hello!";
+	int isocre[2]={0};
+	return 0;
 }
